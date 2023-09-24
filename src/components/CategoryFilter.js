51c5f7b3dff48828c2af10a2135470966f7d6b8b
Yer Yang <yyhmg_86@yahoo.com>
2023-09-24 15:24:54 -0700

    selects a category button when it is clicked

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CategoryFilter({ categories }) {
+  const [selectCategory, setSelectCategory] = useState("All")
+  const onCategoryClicked = (category) => {
+    setSelectCategory(category)
+  }
+
   const displayCategoryList = categories.map((category) => {
-    return <button key={category}>{category}</button>
+    return <button className={`${selectCategory === category ? "selected" : ""}`} key={category} onClick={() => onCategoryClicked(category)
+    }> {category}</button >
   })
 
   return (
