b8d8bcae0d08801e00aca98c7ce9fd78a22d0e95
Yer Yang <yyhmg_86@yahoo.com>
2023-09-24 15:02:12 -0700

    renders <button> elements for each category in the DOM

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories }) {
+  const displayCategoryList = categories.map((category) => {
+    return <button key={category}>{category}</button>
+  })
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {displayCategoryList}
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,7 +4,8 @@ import { v4 as uuid } from "uuid";
 
 function TaskList({ tasks, onDeleteTask }) {
   const displayTaskList = tasks.map((task) => {
-    return <Task key={uuid()} task={task} onDeleteTask={onDeleteTask} />
+    return <Task key={uuid()} task={task} onDeleteTask={onDeleteTask}
+    />
   })
 
   return (
