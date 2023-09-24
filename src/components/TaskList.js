fafdaa9fc020b718f97a0055c5367cde71d1a030
Yer Yang <yyhmg_86@yahoo.com>
2023-09-24 12:00:33 -0700

    displays all tasks in the app and removes the task when the delete button is clicked

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Task from "./Task"
+import { v4 as uuid } from "uuid";
 
-function TaskList() {
+function TaskList({ tasks, onDeleteTask }) {
+  const displayTaskList = tasks.map((task) => {
+    return <Task key={uuid()} task={task} onDeleteTask={onDeleteTask} />
+  })
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {displayTaskList}
     </div>
   );
 }
